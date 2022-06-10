9bb8d7f4c4dd17d729460e61fe0b0ca332f9aa8a
edsonjaramillo <edsonajaramillo@gmail.com>
2022-06-10 13:56:20 -0500

    event json ld added

---
+++ components/events/EventJSONLD.tsx
@@ -0,0 +1,42 @@
+import { AddressManager } from '@/lib/AddressManager';
+import { EventType } from '@/lib/graphcms/types';
+import Head from 'next/head';
+import Script from 'next/script';
+
+const EventJSONLD = (event: EventType) => {
+  const { locationName, address } = new AddressManager().getLocationJSONLD(event);
+
+  const json = {
+    '@context': 'https://schema.org',
+    '@type': 'SocialEvent',
+    name: event.title,
+    startDate: event.startTime,
+    endDate: event.endTime,
+    description: event.description,
+    url: `https://www.mainchannelbrewing.com/events#${event.id}`,
+    // image: 'https://static.tickpick.com/performers/los-angeles-dodgers.jpg',
+    location: {
+      '@type': 'EventVenue',
+      name: locationName,
+      address: {
+        '@type': 'PostalAddress',
+        addressLocality: address.city,
+        addressRegion: address.state,
+        postalCode: address.zipcode,
+        addressCountry: 'US',
+      },
+    },
+    eventStatus: 'https://schema.org/EventScheduled',
+  };
+
+  return (
+    <Head>
+      <script
+        type='application/ld+json'
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(json) }}
+      />
+    </Head>
+  );
+};
+
+export default EventJSONLD;
