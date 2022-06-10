e1e1f4c85194baaa8aadbb595c078f9531b40ab6
edsonjaramillo <edsonajaramillo@gmail.com>
2022-06-10 13:54:52 -0500

    added components index page

---
+++ components/index.tsx
@@ -0,0 +1,40 @@
+// Shared
+export { default as Navbar } from '@/components/shared/Navbar';
+export { default as Section } from '@/components/shared/Section';
+export { default as Footer } from '@/components/shared/Footer';
+export { default as HeadOpenGraph } from '@/components/shared/HeadOpenGraph';
+export { default as BlurImage } from '@/components/shared/BlurImage';
+export { default as PageHeader } from '@/components/shared/PageHeader';
+
+export { default as CTA } from '@/components/home/CTA';
+export { default as Community } from '@/components/home/Community';
+export { default as OurStory } from '@/components/home/OurStory';
+
+export { default as Store } from '@/components/location/Store';
+export { default as Employee } from '@/components/location/Employee';
+export { default as ImageGrid } from '@/components/location/ImageGrid';
+
+export { default as ProductGrid } from '@/components/ontap/ProductGrid';
+export { default as ProductItem } from '@/components/ontap/ProductItem';
+export { default as ProductSection } from '@/components/ontap/ProductSection';
+export { default as PriceItem } from '@/components/ontap/PriceItem';
+export { default as ProductTable } from '@/components/ontap/ProductTable';
+export { default as ProductJSONLD } from '@/components/ontap/ProductJSONLD';
+
+export { default as Event } from '@/components/events/Event';
+export { default as ICal } from '@/components/events/ICal';
+export { default as MapLink } from '@/components/events/MapLink';
+export { default as EventJSONLD } from '@/components/events/EventJSONLD';
+
+export { CustomInput as CustomInput } from '@/components/contact/FormInputs';
+export { TextFieldInput as TextFieldInput } from '@/components/contact/FormInputs';
+export { FormGroupLabel as FormGroupLabel } from '@/components/contact/FormInputs';
+export { RadioInput as RadioInput } from '@/components/contact/FormInputs';
+export { ErrorMessage as ErrorMessage } from '@/components/contact/FormInputs';
+export { default as ContactPageForm } from '@/components/contact/ContactPageForm';
+export { default as DonationPageForm } from '@/components/contact/DonationPageForm';
+export { default as ContactListItem } from '@/components/contact/ContactListItem';
+export { default as ContactIcon } from '@/components/contact/ContactIcon';
+
+export { default as PrivacyPolicy } from '@/components/legal/PrivacyPolicy';
+export { default as Terms } from '@/components/legal/Terms';
