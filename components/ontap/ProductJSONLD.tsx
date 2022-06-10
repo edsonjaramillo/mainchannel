30483b1b0436d54d142b3d4943e1842a59bc71de
edsonjaramillo <edsonajaramillo@gmail.com>
2022-06-10 14:00:12 -0500

    product markup added

---
+++ components/ontap/ProductJSONLD.tsx
@@ -0,0 +1,36 @@
+import { ProductType } from '@/lib/graphcms/types';
+import Head from 'next/head';
+
+const ProductJSONLD = (product: ProductType) => {
+  const productJSON = {
+    '@context': 'https://schema.org',
+    '@type': 'Product',
+    name: product.name,
+    image: product.image.url,
+    description: product.description,
+    brand: {
+      '@type': 'Organization',
+      name: 'Main Channel Brewing',
+    },
+    offers: {
+      '@type': 'Offer',
+      url: `https://www.mainchannelbeer.com/ontap/${product.slug}`,
+      priceCurrency: 'USD',
+      price: product.prices.singlePrice,
+      availability: 'https://schema.org/InStock',
+      itemCondition: 'https://schema.org/NewCondition',
+      availableDeliveryMethod: 'https://schema.org/OnSitePickup',
+    },
+  };
+
+  return (
+    <Head>
+      <script
+        type='application/ld+json'
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(productJSON) }}
+      />
+    </Head>
+  );
+};
+
+export default ProductJSONLD;
