cd8095eb3c1701b796c31025a9502d1a6f471d08
edsonjaramillo <edsonajaramillo@gmail.com>
2022-06-11 16:36:04 -0500

    added formatter functions

--- lib/Formatter.ts
+++ lib/Formatter.ts
@@ -26,10 +26,33 @@ export class Formatter {
     return `$${price.toFixed(2)}`;
   };
 
+  formatDateTime = (date: string): any => {
+    const eventDate = new Date(date);
+
+    const dateOptions: Intl.DateTimeFormatOptions = {
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      timeZone: 'America/Chicago',
+      weekday: 'long',
+    };
+
+    return eventDate.toLocaleDateString('en-US', dateOptions);
+  };
+
   formatPhoneNumber = (phoneNumber: string): string => {
     const areaCode = phoneNumber.slice(0, 3);
     const prefix = phoneNumber.slice(3, 6);
     const lineNumber = phoneNumber.slice(6, 10);
     return `Phone: (${areaCode}) ${prefix}-${lineNumber}`;
   };
+
+  formatEIN = (ein: string): string => {
+    const first = ein.slice(0, 2);
+    const second = ein.slice(2, 9);
+
+    return `${first}-${second}`;
+  }
 }
