6fa6f2e27a8fae33e32ddc4eae7e3f9a6ba52141
edsonjaramillo <edsonajaramillo@gmail.com>
2022-06-11 16:36:40 -0500

    added auth for security

--- lib/graphcms/client.ts
+++ lib/graphcms/client.ts
@@ -1,4 +1,10 @@
 import { GraphQLClient } from 'graphql-request';
 
 const GRAPHCMS_ENDPOINT = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT as string;
-export const graphCMSClient = new GraphQLClient(GRAPHCMS_ENDPOINT);
+const GRAPHCMS_AUTH_TOKEN = process.env.NEXT_PUBLIC_GRAPHCMS_AUTH_TOKEN as string;
+
+export const graphCMSClient = new GraphQLClient(GRAPHCMS_ENDPOINT, {
+  headers: {
+    Authorization: `Bearer ${GRAPHCMS_AUTH_TOKEN}`,
+  },
+});
