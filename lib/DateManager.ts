0d20f284be86f1a939982857d6d800808d5566f1
edsonjaramillo <edsonajaramillo@gmail.com>
2022-06-11 16:35:48 -0500

    refactor date manager

--- lib/DateManager.ts
+++ lib/DateManager.ts
@@ -1,13 +1,13 @@
 export class DateManager {
-  dateAdjuster = (num_days: number): Date => {
+  dateAdjuster = (num_days: number): string => {
     const today = new Date();
     const numericDay: number = today.setDate(today.getDate() + num_days);
-    return new Date(numericDay);
+    return new Date(numericDay).toISOString().split('T')[0] + 'T00:00:00';
   };
 
   getYesterdayISO = (): string => {
     const yesterday = this.dateAdjuster(-1);
-    const date = yesterday.toISOString().split('T')[0];
+    const date = yesterday.split('T')[0];
     return `${date}T00:00:00.000Z`;
   };
 }
