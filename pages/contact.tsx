b1d9860f3e9a503ced6987c529d9712aff783df9
edsonjaramillo <edsonajaramillo@gmail.com>
2022-06-14 11:47:58 -0500

    removed pointers

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -13,11 +13,6 @@ const ContactPage = () => (
         <div className='contact__information'>
           <ContactIcon />
           <h1 className='contact__header'>Lets Talk!</h1>
-          <ul className='contact__list'>
-            <ContactListItem message='Feel free to ask us about our events!' />
-            <ContactListItem message='Lorem ipsum dolor sit, amet consectetur.' />
-            <ContactListItem message='Lorem ipsum dolor sit, amet consectetur.' />
-          </ul>
         </div>
         <div className='contact__form'>
           <ContactPageForm />
