023c179c65c040ecb5d62935034c702aacd61eef
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-10-11 12:38:02 -0500

    added our story section

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -12,18 +12,16 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default async function HomePage() {
-  const cta = await new CMSClient().getCTA();
+  const cms = new CMSClient();
+  const cta = await cms.getCTA();
+  const ourStory = await cms.getOurStory();
 
   return (
     <>
       <CTA cta={cta} />
       <Section headerAs="h2" header="About Us">
         <Text as="p" size="5" textColor="gray" align="center" className="mx-auto max-w-2xl">
-          Founded in 2015, Main Channel Brewing Company got its start as a local microbrewery with humble roots, and has
-          since grown to two locations in Guntersville and Albertville, Alabama. Owners Clay Smith and Bobby & Vickie
-          Satterfield believe in engaging with our local community, and strive to provide a unique experience for all
-          who visit our taprooms. Our goal is to produce high-quality and affordable beer for the growing craft beer
-          community.
+          {ourStory.description}
         </Text>
       </Section>
     </>
