b311eff54add44527853d1068ebdbce74fe4c6e6
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-10-11 12:37:35 -0500

    added bearer token

--- packages/utils/src/env.ts
+++ packages/utils/src/env.ts
@@ -2,4 +2,5 @@ export class ENV {
   static NODE_ENV = process.env.NODE_ENV as 'development' | 'production' | 'test';
   static DOMAIN = process.env.DOMAIN as string;
   static HYGRAPH_ENDPOINT = process.env.HYGRAPH_ENDPOINT as string;
+  static BEARER_TOKEN = process.env.BEARER_TOKEN as string;
 }
