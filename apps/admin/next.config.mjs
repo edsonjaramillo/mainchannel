aef778d548ced9761f3fd46e9c04a27bd4a12fdd
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-11-01 09:00:53 -0500

    removed domains

--- apps/admin/next.config.mjs
+++ apps/admin/next.config.mjs
@@ -1,14 +1,11 @@
 import withPlaiceholder from '@plaiceholder/next';
+
 // import withBundleAnalyzer from '@next/bundle-analyzer';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['ui', 'utils'],
-  images: {
-    domains: ['picsum.photos', 'media.graphassets.com'],
-  },
 };
-
 
 // export default withBundleAnalyzer(nextConfig)
 
