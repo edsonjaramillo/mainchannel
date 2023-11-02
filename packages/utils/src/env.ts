3e4f65641cc058a4637dc8a7322cc95401f2a933
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-11-02 12:34:48 -0500

    Add new environment variables to ENV class

--- packages/utils/src/env.ts
+++ packages/utils/src/env.ts
@@ -1,6 +1,11 @@
 export class ENV {
   static NODE_ENV = process.env.NODE_ENV as 'development' | 'production' | 'test';
-  static DOMAIN = process.env.DOMAIN as string;
+  static NAME = process.env.NAME as string;
   static HYGRAPH_ENDPOINT = process.env.HYGRAPH_ENDPOINT as string;
   static BEARER_TOKEN = process.env.BEARER_TOKEN as string;
+  static EMAILJS_SERVICE_ID = process.env.EMAILJS_SERVICE_ID as string;
+  static EMAILJS_PRIVATE_KEY = process.env.EMAILJS_PRIVATE_KEY as string;
+  static EMAILJS_PUBLIC_KEY = process.env.EMAILJS_PUBLIC_KEY as string;
+  static EMAILJS_CONTACT_FORM_TEMPLATE_ID = process.env.EMAILJS_CONTACT_FORM_TEMPLATE_ID as string;
+  static EMAILJS_DONATION_FORM_TEMPLATE_ID = process.env.EMAILJS_DONATION_FORM_TEMPLATE_ID as string;
 }
