cf7a2e4b510a407f35da7c6c8e2098cfa85dab9d
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-11-02 14:01:28 -0500

    Refactor EventsPage to use Events component

---
+++ apps/web/src/components/events/Events.tsx
@@ -0,0 +1,21 @@
+import { Section } from 'ui/src/Section';
+import { CMSClient } from 'utils/src/cms/CMSClient';
+
+import EventCard from './EventCard';
+
+export const revlalidate = 60 * 60 * 12;
+
+export default async function Events() {
+  const now = new Date().toISOString();
+  const events = await new CMSClient().getEvents(now);
+
+  return (
+    <Section header="Events" headerAs="h1" id="events">
+      <div className="flex flex-col">
+        {events.map((event) => (
+          <EventCard event={event} key={event.id} />
+        ))}
+      </div>
+    </Section>
+  );
+}
