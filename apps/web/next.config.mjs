4b49566742e1b1358a342e48135b9716b59c673b
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-11-02 12:40:20 -0500

    Remove unused import and update image domains in next.config.js.

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,14 +1,14 @@
 import withPlaiceholder from '@plaiceholder/next';
+
 // import withBundleAnalyzer from '@next/bundle-analyzer';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['ui', 'utils'],
   images: {
-    domains: ['picsum.photos', 'media.graphassets.com'],
+    domains: ['media.graphassets.com'],
   },
 };
-
 
 // export default withBundleAnalyzer(nextConfig)
 
