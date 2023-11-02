b0c8c7f7988bd30587a91e4c21323a2d9c1f7758
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-11-02 12:38:32 -0500

    Add donation form schema to validators

--- apps/web/src/validatiors/schemas.ts
+++ apps/web/src/validatiors/schemas.ts
@@ -1,9 +1,19 @@
-import { email, maxLength, minLength, object, string } from 'valibot';
+import { email, maxLength, minLength, object, regex, string } from 'valibot';
 
 export const contactFormSchema = object({
   name: string([minLength(2, 'Must be at least 2 characters long.')]),
   email: string([email('Must be an email.')]),
   phone: string([minLength(10, 'Must be a 10 digit phone number.'), maxLength(10, 'Must be a 10 digit phone number.')]),
   message: string([minLength(2, 'Must be at least 2 characters long.')]),
-  response: string(),
+});
+
+export const donationFormSchema = object({
+  name: string([minLength(2, 'Must be at least 2 characters long.')]),
+  email: string([email('Must be an email.')]),
+  phone: string([minLength(10, 'Must be a 10 digit phone number.'), maxLength(10, 'Must be a 10 digit phone number.')]),
+  location: string([minLength(2, 'Must be at least 2 characters long.')]),
+  date: string([regex(/^\d{4}-\d{2}-\d{2}$/, 'Must be a date.')]),
+  abcnumber: string([regex(/^\d+$/, 'Must be a number.')]),
+  taxid: string([regex(/^\d{9}$/, 'Must be a 9 digit number.')]),
+  message: string([minLength(2, 'Must be at least 2 characters long.')]),
 });
