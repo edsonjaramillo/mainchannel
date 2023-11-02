b56377406826eba30c0f8e4e1648ec6458b3e071
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-11-02 18:58:12 -0500

    Add BUILDING flag to ENV class

--- packages/utils/src/env.ts
+++ packages/utils/src/env.ts
@@ -2,6 +2,7 @@ export class ENV {
   static NODE_ENV = process.env.NODE_ENV as 'development' | 'production' | 'test';
   static NAME = process.env.NAME as string;
   static HYGRAPH_ENDPOINT = process.env.HYGRAPH_ENDPOINT as string;
+  static BUILDING = process.env.BUILDING === 'true' ? true : false;
   static BEARER_TOKEN = process.env.BEARER_TOKEN as string;
   static EMAILJS_SERVICE_ID = process.env.EMAILJS_SERVICE_ID as string;
   static EMAILJS_PRIVATE_KEY = process.env.EMAILJS_PRIVATE_KEY as string;
