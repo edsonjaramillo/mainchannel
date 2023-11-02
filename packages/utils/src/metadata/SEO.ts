3d1c7874f5e38d5cbcaa614d1ec342810df73dd3
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-11-02 12:37:54 -0500

    Refactor SEO metadata generation

--- packages/utils/src/metadata/SEO.ts
+++ packages/utils/src/metadata/SEO.ts
@@ -1,20 +1,20 @@
-import { Metadata } from 'next';
+import type { Metadata } from 'next';
 
 import { ENV } from '../env';
 
-type GenerateMetadata = {
+type RequiredMetadata = {
   title: string;
   description: string;
-  robots?: 'index, follow' | 'noindex, nofollow';
+  robots: 'index, follow' | 'noindex, nofollow';
   canonical?: string;
 } & Metadata;
 
 export class SEO {
-  static async generateMetadata(opts: GenerateMetadata): Promise<Metadata> {
+  static async metadata(opts: RequiredMetadata): Promise<Metadata> {
     return {
-      title: `${opts.title} | ${ENV.DOMAIN}`,
+      title: `${opts.title} | ${ENV.NAME}`,
       description: opts.description,
-      robots: opts.robots ? opts.robots : 'noindex, nofollow',
+      robots: opts.robots,
       alternates: { canonical: opts.canonical },
     };
   }
