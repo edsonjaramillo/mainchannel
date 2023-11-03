d9d7a1821cc4085fb54d6ed7b1ae58baaf411f52
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-11-03 14:24:53 -0500

    Update header in Events component

--- apps/web/src/components/events/Events.tsx
+++ apps/web/src/components/events/Events.tsx
@@ -10,7 +10,7 @@ export default async function Events() {
   const events = await new CMSClient().getEvents(now);
 
   return (
-    <Section header="Events" headerAs="h1" id="events">
+    <Section header="Upcoming" headerAs="h2" id="events">
       <div className="flex flex-col">
         {events.map((event) => (
           <EventCard event={event} key={event.id} />
